b652d02cdfb35b6cb369bfef43efdc3a77324fea
LINGFU ZHANG <lf.zhang86@gmail.com>
2013-03-04 21:37:20 -0700

    update test.js

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
+var static = require('node-static');
 var http = require('http');
 var fs = require('fs');
+var util = require('util');
+var qs = require('querystring');
 var portnumber = 9020;
+var webroot = '.';
+var file = new (static.Server)(webroot, {
+  cache:600,
+  headers: { 'X-Powered-By' : 'node-static'}
+});
 //TODO: support up to 20 connections
-http.globalAgent.maxSockets = 20;
+// http.globalAgent.maxSockets = 20;
 http.createServer(function (req, res) {
+// create chat buffers
+var chatbuffer1;
 // regax match /Chat and parse the chatname and line
 // save the chat message to the buffer
   // set up some routes
@@ -21,14 +31,25 @@ http.createServer(function (req, res) {
     case '/formhandler':
         if (req.method == 'POST') {
             console.log("[200] " + req.method + " to " + req.url);
-            
+            var body = ''
+            // get POST data
             req.on('data', function(chunk) {
               console.log("Received body data:");
+              body += chunk;
               console.log(chunk.toString());
             });
 
+            // parse the chat name
+            var chatname = ''
+            req.on('end', function() {
+              var POST = qs.parse(body);
+              console.log(POST);
+              chatname = POST.chatname;
+              console.log("The chat name is: " + chatname);
+            });
+
             // Display the chat room page
-            fs.readFile('chatRoom.html', function (err, data) {
+            fs.readFile('chatRoom.html?chatname=ttt', function (err, data) {
               if (err)
                 console.log("Error with chatRoom.html");
               res.writeHead(200, {'Content-Type': 'text/html'});
@@ -62,9 +83,24 @@ http.createServer(function (req, res) {
         });
       break;
     default:
-      res.writeHead(404, "Not found", {'Content-Type': 'text/html'});
-      res.end('<html><head><title>404 - Not found</title></head><body><h1>Not found.</h1></body></html>');
-      console.log("[404] " + req.method + " to " + req.url);
+      req.addListener('end', function() {
+        file.serve(req, res, function(err, result) {
+          if (err) {
+            console.error('Error serving %s - %s', req.url, err.message);
+            if (err.status === 404 || err.status === 500) {
+              // file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+            } else {
+              res.writeHead(err.status, err.headers);
+              res.end();
+            }
+          } else {
+            console.log('%s - %s', req.url, res.message);
+          }
+        });
+      });
+      // res.writeHead(404, "Not found", {'Content-Type': 'text/html'});
+      // res.end('<html><head><title>404 - Not found</title></head><body><h1>Not found.</h1></body></html>');
+      // console.log("[404] " + req.method + " to " + req.url);
   };
 
   // Remove users from list when they disconnect
