b8b134739bd1d2ad1c4e7b4cf0878533bfbdfba9
LINGFU ZHANG <lf.zhang86@gmail.com>
2013-03-05 21:19:36 -0700

    update the sample index file and static file server

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require("express"),
     http = require("http"),
     fs = require('fs');
+
+var static = require('node-static');
 // static server
 var webroot = '.';
 var file = new (static.Server)(webroot, {
   cache:600,
   headers: { 'X-Powered-By' : 'node-static'}
 });
-var static = require('node-static');
 
 var app = express();
 var server = http.createServer(app);
@@ -15,29 +16,32 @@ var io = require("socket.io").listen(server);
 
 io.configure(function () { 
     io.set("transports", ["xhr-polling"]); 
-    io.set("polling duration", 10); 
+    io.set("polling duration", 60); 
+});
+
+app.get("/", function (req, res) {
+    res.sendfile(__dirname + "/index.html");
+});
+
+app.get("/Chat", function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<html><body><p>The req is : " + req + "</p></body></html>");
+    console.log("********************************" + req);
+});
+
+app.configure(function() {
+    // app.use(express.methodOverride());
+    // app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/'));
+    // app.use(express.errorHandler({
+    //     dumpExceptions: true, 
+    //     showStack: true
+    // }));
+    // app.use(app.router);
 });
 
 var port = 9020;
 server.listen(port);
-
-app.get("/", function (req, res) {
-    res.sendfile(__dirname + "/index.html");
-    req.addListener('end', function() {
-        file.serve(req, res, function(err, result) {
-          if (err) {
-            console.error('Error serving %s - %s', req.url, err.message);
-            if (err.status === 404 || err.status === 500) {
-              // file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
-            } else {
-              res.writeHead(err.status, err.headers);
-              res.end();
-            }
-          } else {
-            console.log('%s - %s', req.url, res.message);
-          }
-        });
-});
 
 io.sockets.on("connection", function (socket) {
     socket.emit("from server", { message: "Welcome to Larry and Thang's Chat Room!" });
@@ -60,4 +64,3 @@ function sendAll(message, user) {
             io.sockets.sockets[socket].emit("from server", message);
     }
 }
-
